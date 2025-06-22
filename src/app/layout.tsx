45b3f2d84335b887581a67efaa6dbaa927e69a63
evelynhelena <evelynhelena781@gmail.com>
2025-06-22 19:33:59 -0300

    arrumando layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,11 @@
-
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import 'remixicon/fonts/remixicon.css'
+
 import Header from "@/components/Header";
+import { Inter } from "next/font/google";
 import Menu from "@/components/Menu";
-
+import type { Metadata } from "next";
 import ReactQueryProvider from "@/components/ReactQueryProvider";
-
 
 const inter = Inter({
   weight: ['400', '500', '300', '700'],
@@ -34,7 +32,7 @@ export default function RootLayout({
             <Header />
             <Menu />
           </div>
-          <div className="flex justify-end p-20">
+          <div className="flex justify-end md:p-20 p-5 max-md:pt-20">
             <div className="w-full md:w-[calc(100%-246px)]">
               {children}
             </div>
